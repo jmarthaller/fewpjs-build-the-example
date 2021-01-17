92c711b42947360850298f98ee4440c199ef5254
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-17 14:28:38 -0500

    Done.

--- main.js
+++ main.js
@@ -5,8 +5,41 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+// const heartBtn = document.querySelector('.like-glyph')
+const likes = document.querySelectorAll('li')
+const modal = document.querySelector('#modal')
+const articles = document.querySelector('#article-container')
 
-//mimicServerCall()
+
+
+articles.addEventListener('click', function(e) {
+
+  const heartSpan = e.target.closest('span')
+  mimicServerCall()
+    .then(function(serverMessage) {
+      alert("You notified the server")
+      alert(serverMessage)
+      if (heartSpan.textContent === EMPTY_HEART) {
+        heartSpan.textContent = FULL_HEART
+        heartSpan.classList.add('activated-heart')
+      } else {
+        heartSpan.textContent = EMPTY_HEART
+        heartSpan.classList.remove('activated-heart')
+      }
+    }) 
+    .catch(function(error) {
+      modal.classList.toggle('hidden')
+      setTimeout(function() {
+        modal.remove()
+      }, 5000)
+    })
+
+})
+
+
+
+
+
 
 
 
